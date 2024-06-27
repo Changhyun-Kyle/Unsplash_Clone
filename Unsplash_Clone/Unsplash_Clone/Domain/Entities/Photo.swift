b74c4a89506bb0de4e4d9ee3b3685b0fd3f21d45
Changhyun Kang <changhyun.kyle@gmail.com>
2024-06-27 21:07:43 +0900

    chore: Clean Architecture 설계를 위한 폴더링 및 레이어 별 파일 생성

---
+++ Unsplash_Clone/Unsplash_Clone/Domain/Entities/Photo.swift
@@ -0,0 +1,8 @@
+//
+//  Photo.swift
+//  Unsplash_Clone
+//
+//  Created by 강창현 on 6/27/24.
+//
+
+import Foundation
---
+++ Unsplash_Clone/Unsplash_Clone/Domain/UseCases/FetchPhotosUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  FetchPhotosUseCase.swift
+//  Unsplash_Clone
+//
+//  Created by 강창현 on 6/27/24.
+//
+
+import Foundation
