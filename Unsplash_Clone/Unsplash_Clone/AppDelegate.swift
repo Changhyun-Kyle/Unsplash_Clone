9b803d1d718624b66c0ea8498da1e05789901303
Changhyun Kang <changhyun.kyle@gmail.com>
2024-06-27 17:40:50 +0900

    config: 프로젝트 생성 및 초기 설정

---
+++ Unsplash_Clone/Unsplash_Clone/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  Unsplash_Clone
+//
+//  Created by 강창현 on 6/27/24.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) { }
+}
---
+++ Unsplash_Clone/Unsplash_Clone/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Unsplash_Clone
+//
+//  Created by 강창현 on 6/27/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
